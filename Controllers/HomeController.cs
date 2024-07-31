3fd5e2a17af171a6b3b3e59b4843235c6d7be2f4
Abhishek Alandikar <95140474+abhishekalandikar@users.noreply.github.com>
2024-07-31 13:06:13 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Web.Mvc;
+using MvcYoutube.Models;
+
+public class HomeController : Controller
+{
+    private readonly VideoEntities _context;
+
+    public HomeController()
+    {
+        _context = new VideoEntities();
+    }
+
+    [HttpPost] //
+    public ActionResult TrackAnalytics(Analytic analytics)
+    {
+        if (ModelState.IsValid)
+        {
+            // Calculate the watched time as a float, for example:
+            float watchedTimeInSeconds = (float)(DateTime.UtcNow.ToOADate() - analytics.Timestamp.ToOADate());
+            analytics.WatchedTime = watchedTimeInSeconds;
+            analytics.Timestamp = DateTime.UtcNow;
+
+            _context.Analytics.Add(analytics);
+            _context.SaveChanges();
+
+            return new HttpStatusCodeResult(200); // OK
+        }
+        return new HttpStatusCodeResult(400); // Bad Request
+    }
+
+
+    public ActionResult Index()
+    {
+        ViewBag.Title = "YouTube Video Player";
+        return View();
+    }
+}
---
+++ Models/Video.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcYoutube.Models
+{
+    public class Video
+    {
+        public int Id { get; set; } 
+        public string VideoId { get; set; }
+        public float WatchedTime { get; set; }
+        public DateTime Timestamp { get; set; }
+
+    }
+}
